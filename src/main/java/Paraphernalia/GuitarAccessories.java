2197121cd6d1b87a9bd8aab5bcc4cd5e55fbbffd
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-07-07 09:54:14 +0100

    got enum accessories working

--- src/main/java/Paraphernalia/GuitarAccessories.java
+++ src/main/java/Paraphernalia/GuitarAccessories.java
@@ -1,4 +1,14 @@
 package Paraphernalia;
 
-public class GuitarAccessories {
+public class GuitarAccessories extends Paraphernalia {
+    private GuitarAccessoryType type;
+
+    public GuitarAccessories(GuitarAccessoryType type, double stockPrice, double retailPrice, String productDescription){
+        super(stockPrice, retailPrice, productDescription);
+        this.type = type;
+    }
+
+    public GuitarAccessoryType getStrings() {
+        return this.type;
+    }
 }
--- src/main/java/Paraphernalia/GuitarAccessoryType.java
+++ src/main/java/Paraphernalia/GuitarAccessoryType.java
@@ -1,9 +1,14 @@
 package Paraphernalia;
 
 public enum GuitarAccessoryType {
+    STRINGS("Strings"),
+    PICKS("Picks"),
+    TUNER("Tuner"),
+    STAND("Stand");
+
     private final String type;
 
-    InstrumentType(String type){
+    GuitarAccessoryType(String type){
         this.type = type;
     }
 
